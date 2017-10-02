7b37f339b940ec83691d899e7b8b1dc6f23e56a7
Your Name <you@example.com>
2017-10-02 09:14:29 -0700

    only able to finsih 2

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,9 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+	$('span').on('click', function count(num){
+		var itemClick = $(this).text() + " at " + Date.now() ;
+		$("ul").append("<li>" + itemClick + "</li>")
+	})
 
 })
